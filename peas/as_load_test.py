7a107c92c202c5c21db0348c9aca09edcc300386
Mingfa Ma <mingfa.ma@deepnetsecurity.com>
2021-02-21 11:26:47 +0000

    load test file

---
+++ peas/as_load_test.py
@@ -0,0 +1,161 @@
+#! /usr/bin/python
+
+import subprocess
+import threading
+import time
+import datetime
+import pathlib
+import argparse
+
+now=str(datetime.datetime.now())
+subject='Stress test (%s)' % now
+base=10000
+global quiet
+
+quiet=False
+
+class AtomicInteger():
+    def __init__(self, value=0):
+        self._value = int(value)
+        self._lock = threading.Lock()
+
+    def inc(self, d=1):
+        with self._lock:
+            self._value += int(d)
+            return self._value
+
+    def dec(self, d=1):
+        return self.inc(-d)
+
+    @property
+    def value(self):
+        with self._lock:
+            return self._value
+
+    @value.setter
+    def value(self, v):
+        with self._lock:
+            self._value = int(v)
+
+
+class AtomicLog():
+    def __init__(self, file_name):
+        self._f = open(file_name, "wt")
+        self._lock = threading.Lock()
+
+    def write(self,msg):
+        with self._lock:
+            self._f.write(msg)
+    def close(self):
+        self._f.close()
+
+succeeded = AtomicInteger()
+failed = AtomicInteger()
+executed = AtomicInteger()
+global log
+#log = None
+
+def test_thread_function(domain, password, index, begin, end, sleep, period):
+    tic = time.perf_counter()
+    toc = tic
+    time.sleep(index*sleep)
+    while True:
+        for i in range(begin, end):
+            email="L%d.T%d@%s" % (i,i,domain)
+            user=email
+            args=['ruler', "-k", "--email", email, "--username", user, "--password", password, "send", "--subject", subject]
+            # Check credentials
+            # python -m peas -u 'MEGACORP\s.freeside' -p 'Passw0rd1!' mx.megacorp.local --check
+            ticrun = time.perf_counter()
+            ret = subprocess.run(args=args, text=True, capture_output=True)
+            toc = time.perf_counter()
+            if not quiet:
+                print("thread {%03d}: %s %0.2fs %0.2fs %d" % (index, email, toc-tic, toc-ticrun, ret.returncode))
+            if ret.returncode==0:
+                succeeded.inc()
+            else:
+                failed.inc()
+                log.write("thread {%d}: %d %s\n" % (index,ret.returncode,' '.join(ret.args)))
+                log.write(ret.stderr)
+            executed.inc()
+            time.sleep(sleep)
+            if period is not None and (toc-tic)/60 > period:
+                return
+        if period is None:
+            return
+
+def test_main(args):
+    tic = time.perf_counter()
+    delay = args.delay
+    accounts = args.accounts
+    domain = args.domain
+    period = args.period
+    password = args.password
+    global quiet
+    quiet = args.quiet
+
+    # prepare test threads
+    threads = []
+
+    global log
+    log = AtomicLog(args.log)
+
+    print("======== Preparing threads ================")
+
+    for i in range(0, accounts):
+        begin = base + i
+        end = begin + 1
+        print("thread %d, %d -> %d" % (i, begin, end))
+        threads.append(threading.Thread(target=test_thread_function, args=(domain, password, i, begin, end, delay, period)))
+
+    # run all tests
+    print("======== Start threads ====================")
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    print("======== Finished =========================")
+
+    # print summary
+    toc = time.perf_counter()
+    print("threads: %d" % accounts)
+    print("delay: %f" % delay)
+    if period is not None:
+        print("period: %d" % period)
+    print("")
+    print("accounts: %d" % accounts)
+    print("succeeded: %d" % succeeded.value)
+    print("failed: %d" % failed.value)
+    print("requests/second: %0.4f" % (float(succeeded.value + failed.value)/(toc - tic)) )
+    print(f"Tests finished in {toc - tic:0.4f} seconds")
+
+    # generate test report
+    report_file = pathlib.Path(args.report)
+    is_old = report_file.is_file()
+
+    f=open(args.report, "at")
+    if not is_old:
+        f.write('timestamp,threads,delay,accounts,succeeded,failed,time\n') # write csv header
+    f.write('"%s","%d","%f","%d","%d","%d","%f"\n' % (now,accounts,delay,accounts,succeeded.value,failed.value, toc - tic))
+    f.close()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='ActiveSync load tester')
+# For simplicity, threads = accounts,
+#    parser.add_argument('--threads', help='number of concurrent threads',type=int, required=True)
+    parser.add_argument('--delay', default=0.1, help='the delay (in seconds) between each requests in a thread, default value: 0.1 (seconds)',type=float)
+    parser.add_argument('--accounts', help='the number of accounts to be tested',type=int, required=True)
+    parser.add_argument('--domain', help='the test domain, e.g. yourdomain.com', required=True)
+    parser.add_argument('--password', help='mailbox password', required=True)
+    parser.add_argument('--period', help='test period, in minutes, optional', type=int)
+    parser.add_argument('--report', default="report.csv", help='report file name, default name: report.csv')
+    parser.add_argument('--quiet', action=argparse.BooleanOptionalAction, help='do not show result of every request')
+    parser.add_argument('--log', default="activesync_test.log", help='error log file name, default name: activesync_test.log')
+
+    args = parser.parse_args()
+    test_main(args)
