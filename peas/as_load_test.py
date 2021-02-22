2f0d430e2ce362d27b485f80c5dc4ea89f7b718f
Mingfa Ma <mingfa.ma@deepnetsecurity.com>
2021-02-22 09:36:10 +0000

    add user for single test

--- peas/as_load_test.py
+++ peas/as_load_test.py
@@ -10,12 +10,13 @@ import sys
 
 from peas import Peas
 
-now=str(datetime.datetime.now())
-subject='Stress test (%s)' % now
-base=10000
+now = str(datetime.datetime.now())
+subject = 'Stress test (%s)' % now
+base = 10000
 global quiet
 
-quiet=False
+quiet = False
+
 
 class AtomicInteger():
     def __init__(self, value=0):
@@ -46,23 +47,25 @@ class AtomicLog():
         self._f = open(file_name, "wt")
         self._lock = threading.Lock()
 
-    def write(self,msg):
+    def write(self, msg):
         with self._lock:
             self._f.write(msg)
+
     def close(self):
         self._f.close()
+
 
 succeeded = AtomicInteger()
 failed = AtomicInteger()
 executed = AtomicInteger()
 global log
-#log = None
+# log = None
 
 R = '\033[1;31m'  # RED
 G = '\033[0;32m'  # GREEN
 Y = '\033[0;33m'  # YELLOW
 M = '\033[0;35m'  # MAGENTA
-S = '\033[0m'     # RESET
+S = '\033[0m'  # RESET
 
 
 def error(msg):
@@ -70,7 +73,6 @@ def error(msg):
 
 
 def init_authed_client(server, user, password, verify=True):
-
     if user is None:
         error("A username must be specified for this command.")
         return False
@@ -100,16 +102,16 @@ def init_authed_client(server, user, password, verify=True):
     return client
 
 
-def test_thread_function(server, domain, password, index, begin, end, sleep, period):
+def test_thread_function(server, domain, singleuser, password, index, begin, end, sleep, period):
     tic = time.perf_counter()
     toc = tic
-    time.sleep(index*sleep)
+    time.sleep(index * sleep)
     while True:
         for i in range(begin, end):
-            email="L%d.T%d@%s" % (i,i,domain)
-            user=email
-            args=['ruler', "-k", "--email", email, "--username", user, "--password", password, "send", "--subject", subject]
-
+            email = "L%d.T%d@%s" % (i, i, domain)
+            user = email
+            if singleuser is not None:
+                user = singleuser
             ticrun = time.perf_counter()
             client = init_authed_client(server, user, password, False)
             if not client:
@@ -119,7 +121,7 @@ def test_thread_function(server, domain, password, index, begin, end, sleep, per
 
             toc = time.perf_counter()
             if not quiet:
-                print("thread {%03d}: %s %0.2fs %0.2fs" % (index, email, toc-tic, toc-ticrun))
+                print("thread {%03d}: %s %0.2fs %0.2fs" % (index, email, toc - tic, toc - ticrun))
             if creds_valid:
                 succeeded.inc()
             else:
@@ -127,10 +129,11 @@ def test_thread_function(server, domain, password, index, begin, end, sleep, per
 
             executed.inc()
             time.sleep(sleep)
-            if period is not None and (toc-tic)/60 > period:
+            if period is not None and (toc - tic) / 60 > period:
                 return
         if period is None:
             return
+
 
 def test_main(args):
     tic = time.perf_counter()
@@ -140,6 +143,10 @@ def test_main(args):
     domain = args.domain
     period = args.period
     password = args.password
+    singleuser = args.user
+
+    if singleuser is not None:
+        accounts = 1
 
     global quiet
     quiet = args.quiet
@@ -156,7 +163,8 @@ def test_main(args):
         begin = base + i
         end = begin + 1
         print("thread %d, %d -> %d" % (i, begin, end))
-        threads.append(threading.Thread(target=test_thread_function, args=(server, domain, password, i, begin, end, delay, period)))
+        threads.append(threading.Thread(target=test_thread_function,
+                                        args=(server, domain, singleuser, password, i, begin, end, delay, period)))
 
     # run all tests
     print("======== Start threads ====================")
@@ -179,34 +187,38 @@ def test_main(args):
     print("accounts: %d" % accounts)
     print("succeeded: %d" % succeeded.value)
     print("failed: %d" % failed.value)
-    print("requests/second: %0.4f" % (float(succeeded.value + failed.value)/(toc - tic)) )
+    print("requests/second: %0.4f" % (float(succeeded.value + failed.value) / (toc - tic)))
     print(f"Tests finished in {toc - tic:0.4f} seconds")
 
     # generate test report
     report_file = pathlib.Path(args.report)
     is_old = report_file.is_file()
 
-    f=open(args.report, "at")
+    f = open(args.report, "at")
     if not is_old:
-        f.write('timestamp,threads,delay,accounts,succeeded,failed,time\n') # write csv header
-    f.write('"%s","%d","%f","%d","%d","%d","%f"\n' % (now,accounts,delay,accounts,succeeded.value,failed.value, toc - tic))
+        f.write('timestamp,threads,delay,accounts,succeeded,failed,time\n')  # write csv header
+    f.write('"%s","%d","%f","%d","%d","%d","%f"\n' % (
+    now, accounts, delay, accounts, succeeded.value, failed.value, toc - tic))
     f.close()
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description='ActiveSync load tester')
-# For simplicity, threads = accounts,
-#    parser.add_argument('--threads', help='number of concurrent threads',type=int, required=True)
-    parser.add_argument('--delay', default=0.1, help='the delay (in seconds) between each requests in a thread, default value: 0.1 (seconds)',type=float)
-    parser.add_argument('--accounts', help='the number of accounts to be tested',type=int, required=True)
+    # For simplicity, threads = accounts,
+    #    parser.add_argument('--threads', help='number of concurrent threads',type=int, required=True)
+    parser.add_argument('--delay', default=0.1,
+                        help='the delay (in seconds) between each requests in a thread, default value: 0.1 (seconds)',
+                        type=float)
+    parser.add_argument('--accounts', help='the number of accounts to be tested', type=int, required=True)
     parser.add_argument('--domain', help='the test domain, e.g. yourdomain.com', required=True)
     parser.add_argument('--server', help='the exchange server, e.g. mail.yourdomain.com', required=True)
     parser.add_argument('--password', help='mailbox password', required=True)
+    parser.add_argument('--user', help='username for single test, optional')
     parser.add_argument('--period', help='test period, in minutes, optional', type=int)
     parser.add_argument('--report', default="report.csv", help='report file name, default name: report.csv')
     parser.add_argument('--quiet', action=argparse.BooleanOptionalAction, help='do not show result of every request')
-    parser.add_argument('--log', default="activesync_test.log", help='error log file name, default name: activesync_test.log')
+    parser.add_argument('--log', default="activesync_test.log",
+                        help='error log file name, default name: activesync_test.log')
 
     args = parser.parse_args()
     test_main(args)
